74761b9c2a99b0287a2c651bcfe60b5450d9822e
ksan09 <kansan060223@gmail.com>
2024-05-29 12:48:04 +0900

    what

--- suhang_the_second/Assets/01.Scripts/Core/IDamageable.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamageable
-{
-    public void OnDamage(int damage, Vector3 point, Vector3 normal);
-}
